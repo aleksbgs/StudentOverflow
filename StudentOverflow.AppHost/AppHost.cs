405eeb002d7e7b86a92cbbcfe1786ff9eeb079bd
Aleksandar Markovic <amarkovic29@gmail.com>
2026-03-10 21:04:52 +0100

    End of Section 3

--- StudentOverflow.AppHost/AppHost.cs
+++ StudentOverflow.AppHost/AppHost.cs
@@ -9,6 +9,21 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
 
     .WithDataVolume("keycloak-data");
 
+
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("questionDb");
+
+
+var questionService = builder.AddProject<Projects.QuestionService>("QuestionService")
+    .WithReference(keycloak)
+    .WithReference(questionDb)
+    .WaitFor(questionDb)
+    .WaitFor(keycloak);
+
+
 #pragma warning restore ASPIRECERTIFICATES001
 
 builder.Build().Run();
